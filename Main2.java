4b56747aa1acf853abe734134f34b839521e9409
Orr16 <orr.king123@gmail.com>
2025-12-15 15:21:22 +0200

    Update Main2.java

--- Main2.java
+++ Main2.java
@@ -1,23 +1,36 @@
 public class Main {
     public static int def1(Queue<Integer> a, int x) {
-        int i  = 0;
-        int n = 0;
+        Queue<Integer> copy = copyQueue(a);
         int z = 0;
-        while (!a.isEmpty()) {
-            n = a.remove();
+        while (!copy.isEmpty()) {
+            int n = copy.remove();
             z = z + n;
-            int count++;
         }
         return z;
     }
 
     public static int count(Queue<Integer> a) {
+        Queue<Integer> copy = copyQueue(a);
         int count = 0;
-        while (!a.isEmpty()) {
-            a.remove();
+        while (!copy.isEmpty()) {
+            copy.remove();
             count++;
         }
         return count;
+    }
+
+    public static Queue<Integer> copyQueue(Queue<Integer> original) {
+        Queue<Integer> copy = new Queue<>();
+        Queue<Integer> temp = new Queue<>();
+        while (!original.isEmpty()) {
+            int value = original.remove();
+            temp.insert(value);
+            copy.insert(value);
+        }
+        while (!temp.isEmpty()) {
+            original.insert(temp.remove());
+        }
+        return copy;
     }
 
     public static void main(String[] args) {
